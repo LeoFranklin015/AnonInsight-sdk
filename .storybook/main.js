395aa926b9a01d521a341eddb4dccc563a057565
LeoFranklin015 <leofranklinjohn.25cs@licet.ac.in>
2024-02-17 15:44:56 +0530

    build : sindri proof added

--- .storybook/main.js
+++ .storybook/main.js
@@ -3,12 +3,25 @@ const path = require('path');
 module.exports = {
   stories: ['../src/**/*.stories.tsx'],
   webpackFinal: async (config, { configType }) => {
-   // add scss support
-    config.module.rules.push({
-      test: /\.scss$/,
-      use: ['style-loader', 'css-loader', 'sass-loader'],
-      include: path.resolve(__dirname, '../'),
-    });
+    // add scss support
+    config.module.rules.push(
+      {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+        include: path.resolve(__dirname, '../'),
+      },
+      {
+        test: /\.m?js$/,
+        include: /node_modules/,
+        type: 'javascript/auto',
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      }
+    );
 
     return config;
   },
